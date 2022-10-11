faf64fc67e0bdcc51edc9b9fbcc0fac1b6ff129f
Amanda Maciel <amandamacielj@gmail.com>
2022-10-11 08:58:12 -0300

    segundo commit entrada dos dados

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+    int N; //Tamanho da série/vetor
+    float *vet = malloc(sizeof(float)*N); //Definindo o tamanho do vetor a partir do número digitado pelo usuário
+
+    printf("\nQuantos numeros na sua serie (N): ");
+    scanf("%d", &N);
+
+    for(int i=0; i<N; i++){
+    printf("Entre com numeros: ");
+    scanf("%f", &vet[0]);
+    }
+
     return 0;
+
 }
+
