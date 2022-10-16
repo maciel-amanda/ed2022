d75b0c77633863314c404f9e759ca589dca80fd1
Amanda Maciel <amandamacielj@gmail.com>
2022-10-16 16:04:12 -0300

    4º commit desvio padrão

--- main.c
+++ main.c
@@ -3,75 +3,77 @@
 #include <math.h>
 
 int main(){
-    //Definindo entrada de dados:
+/*====ENTRADA DE DADOS==============================================================================================================*/
 
-    int N; //Tamanho da vetor/série
-    float *vet = malloc(sizeof(float)*N); //Definindo o tamanho do vetor a partir do número digitado pelo usuário. considerando o tamanho de nºs float
+    int N; // Variável referente à série/vetor.
+    float *vet = malloc(sizeof(float)*N); // Define o tamanho do vetor e o nº de bytes a serem alocados em memória.
 
-    printf("\nQuantos numeros na sua serie (N): ");
-    scanf("%d", &N); //Função que capta a entrada referente ao tamanho do vetor
+    printf("Quantos numeros na sua serie (N): ");
+    scanf("%d", &N); // Função que capta a entrada de dados do usuário e define o tamanho da série/vetor.
 
-    printf("\nEntre com numeros: ");
-    for(int i=0; i<N; i++){ //"for" para caminhar pelos índices do vetor agregando os valores digitados pelo unuário
-    scanf("%f", &vet[i]);//função que capta todos os números que irão compor a série/vetor (preenchendo os índices)
-
+    printf("\nEntre com numeros: \n");
+    for(int i=0; i<N; i++){ // Laço que percorre todos os ídices do vetor.
+    scanf("%f", &vet[i]); //Função que capta as entradas de nºs da série/vetor e aloca em índices.
     }
-    //Maior e Menor
+
+/*====MAIOR E MENOR=================================================================================================================*/
 
     float maior= vet[0];
     float menor= vet[0];
-    for(int i=0; i<N; i++){
-        if (vet[i]<menor)
+
+    for(int i=0; i<N; i++){ // Laço que percorre os índices do vetor.
+        if (vet[i]<menor) // Compara os valores defindindo o menor.
         menor=vet[i];
-        if(vet[i]>maior)
+        if(vet[i]>maior) // compara os valores definindo o maior.
         maior=vet[i];
         }
     printf("\nValor minimo: %.1f\nValor maximo: %.1f\n", menor, maior);
 
-    // Média
+/*====MÉDIA=========================================================================================================================*/
 
     float soma = 0;
     float media = 0;
-    for (int i=0; i<N; i++){
-        soma = soma+vet[i];
+
+    for (int i=0; i<N; i++){ // Laço que percorre os indices.
+        soma = soma+vet[i]; // Soma os valores em cada índice.
     }
-    media = soma/N;
+    media = soma/N; // Cálculo da média.
     printf("Media: %.2f\n", media);
 
-    //mediana
+/*====MEDIANA=======================================================================================================================*/
+
     float mediana = 0;
-     for(int i=0;i<N-1;i++){
-        for(int j=i+1;j<N;j++){
+
+     for(int i=0; i<N-1; i++){ // Ordenação dos vetores (comparando o vetor original e ordenado).
+        for(int j=i+1; j<N; j++){
 
             if(vet[i] > vet[j]){
 
-                int aux = vet[i];
+                float aux = vet[i];
                 vet[i] = vet[j];
                 vet[j] = aux;
             }
         }
     }
-    if(N%2){
-         mediana = vet[N/2];
+    if(N%2){ // Considera o resto da divisão por 2.
+         mediana = vet[N/2]; // Se N par considera o valor central.
     } else {
-         mediana = (vet[N/2-1]+vet[N/2])/2;
+         mediana = (vet[N/2-1]+vet[N/2])/2;// Se N ímpar calcula média dos dois valores centrais.
     }
     printf("Mediana: %.2f \n", mediana);
 
-   // Desvio padrão
 
+/*====DESVIO PADRÃO (POPULACIONAL)==================================================================================================*/
 
+      float variancia = 0;
+
+      for (int i=0; i<N; i++) { // laço que percorre os números nos índices.
+        float num = vet[i] - media; // Subtrai os números (nos índices) da média => desvios.
+        variancia += num * num; // Calcula o quadrado dos desvios (multiplando cada número por ele mesmo) e depois soma.
+      }
+       float dp = sqrt(variancia/N); // Cálculo da raiz quadrada da variância dividida pelo total de números da série(N).
+       printf("Desvio Padrao Populacional = %.13f\n", dp);
 
 return 0;
-
-
-
-
-
-
-
-
-
-
 
 }
